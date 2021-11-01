1f96e4e2c4b4fa6f645be057914fe9d1286d4975
Jimmy Engström <jimmy@apeoholic.se>
2021-11-01 20:13:24 +0100

    Fixed reconnect issues

--- Blazm.Bluetooth/wwwroot/Blazm.Bluetooth.js
+++ Blazm.Bluetooth/wwwroot/Blazm.Bluetooth.js
@@ -5,22 +5,21 @@ export async function requestDevice(query)
     console.log(query);
 
     var device = await navigator.bluetooth.requestDevice(objquery);
-    console.log(device);
     await device.gatt.connect();
     device.addEventListener('gattserverdisconnected', onDisconnected);
     PairedBluetoothDevices.push(device);
-    console.log(device.id);
     return { "Name": device.name, "Id": device.id };    
 }
 
 
-export async function  onDisconnected() {
+export async function  onDisconnected(arg) {
+    console.log(arg.srcElement);
     console.log('> Bluetooth Device disconnected');
-    connect();
+    connect(arg.srcElement);
 }
 
 function connect(bluetoothDevice) {
-    exponentialBackoff(3 /* max retries */, 2 /* seconds delay */,
+    exponentialBackoff(3 /* max retries */, 1 /* seconds delay */,
         function toTry() {
             time('Connecting to Bluetooth Device... ');
             return bluetoothDevice.gatt.connect();
@@ -60,12 +59,22 @@ function getDevice(deviceId) {
 export async function writeValue(deviceId, serviceId, characteristicId, value)
 {
     var device = getDevice(deviceId);
+    console.log(device);
+    if (device.gatt.connected) {
+        var service = await device.gatt.getPrimaryService(serviceId);
+        var characteristic = await service.getCharacteristic(characteristicId);
+        var b = Uint8Array.from(value);
+        await characteristic.writeValue(b);
+    }
+    else
+    {
+        await sleep(1000);
+        await writeValue(deviceId, serviceId, characteristicId, value);
+    }
+}
 
-    var service = await device[0].gatt.getPrimaryService(serviceId);
-    var characteristic = await service.getCharacteristic(characteristicId);
-    var b = Uint8Array.from(value);
-    await characteristic.writeValue(b);
-        
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 export async function readValue(deviceId, serviceId, characteristicId)
